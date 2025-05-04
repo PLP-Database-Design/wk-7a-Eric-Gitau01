d2f9e8ac458e9f0248083d224b72f1181e39eaa7
Eric-Gitau01 <gitaueric01@gmail.com>
2025-05-04 14:27:26 +0300

    redone the assignment week 7

--- answers.sql
+++ answers.sql
@@ -1,40 +1,37 @@
--- question 1
-
-CREATE TABLE ProductDetail_1NF (
+--Question 1
+CREATE TABLE productDetail_1NF (
     OrderID INT,
     CustomerName VARCHAR(100),
-    Product VARCHAR(50) 
+    Product VARCHAR(50)
 );
 
-INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
-VALUES 
+INSERT INTO productDetail_1NF (OrderID, CustomerName, Product) VALUES
     (101, 'John Doe', 'Laptop'),
     (101, 'John Doe', 'Mouse'),
-    (102, 'Jane Smith', 'Tablet'),
-    (102, 'Jane Smith', 'Keyboard'),
-    (102, 'Jane Smith', 'Mouse'),
-    (103, 'Emily Clark', 'Phone');
+    (101, 'Jane Smith', 'Tablet'),
+    (101, 'Jane Smith', 'Keyboard'),
+    (101, 'Jane Smith', 'Mouse'),
+    (101, 'Emily Clark', 'Phone');
 
 --question 2
-table (removes CustomerName partial dependency)
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
-);
+)
 
-
-CREATE TABLE OrderItems (
+CREATE TABLE OrderDetails_2NF (
     OrderID INT,
     Product VARCHAR(50),
-    Quantity INT,
+    Quality INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName FROM OrderDetails;
-
-INSERT INTO OrderItems (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity FROM OrderDetails;
-
--- 
+INSERT INTO OrderDetails_2NF (OrderID, Product, Quality) VALUES
+    (101, 'laptop', 2),
+    (101, 'Mouse', 1),
+    (101, 'Tablet', 3),
+    (101, 'Keyboard', 1),
+    (101, 'Mouse', 2),
+    (101, 'Mouse', 2),
+    (101, 'Phone', 2);
